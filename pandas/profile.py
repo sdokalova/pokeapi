945b16f6bf351c0560aaf4219d469310f76e3ec2
Sofia Dokalova <sofiya.dokalova@quantori.com>
2024-07-22 21:25:21 +0300

    Add tests

--- pandas/profile.py
+++ pandas/profile.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
+import numpy as np
 
-data = pd.read_csv("pandas/netflix_titles.csv", encoding='unicode_escape')
+data = pd.read_csv("pandas/netflix_titles.csv", encoding='unicode_escape').replace({np.nan: None})
 
 profile = ProfileReport(data, title="Profiling Report")
 profile.to_file("report.html")
--- pandas/tests.py
+++ pandas/tests.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import pytest
-import re
 import numpy as np
+import datetime as dt
+
 
 data = pd.read_csv("netflix_titles.csv", encoding='unicode_escape')
 expected_columns = [
     "show_id", "type", "title", "director", "cast", "country", "date_added", "release_year", "rating",
     "duration", "listed_in", "description"
 ]
-categories = ["A", "G", "NC-17", "NR", "PG", "PG-13", "R", "TV-14", "TV-G", "TV-MA", "TV-PG", "TV-Y", "TV-Y7", "TV-Y7-FV", "UR"]
+categories = [
+    "A", "G", "NC-17", "NR", "PG", "PG-13", "R", "TV-14", "TV-G", "TV-MA", "TV-PG", "TV-Y", "TV-Y7", "TV-Y7-FV", "UR"
+]
 
 
 def test_schema():
@@ -16,7 +19,7 @@ def test_schema():
     assert sorted(data.columns.values.tolist()) == sorted(expected_columns)
 
 
-cleaned_data = data[expected_columns]
+cleaned_data = data.dropna(how='all', axis=1).replace({np.nan: None})
 
 
 @pytest.mark.parametrize("column", ["show_id", "title", "release_year"])
@@ -57,9 +60,27 @@ def test_category():
 
 def test_updates():
     """Determine if the dataset includes recently added titles."""
-    filtered_df = cleaned_data['date_added'][~cleaned_data['date_added'].isna()]
-    assert not filtered_df[filtered_df.str.endswith('2023')].empty
+    filtered_df = cleaned_data["date_added"][~cleaned_data["date_added"].isna()]
+    assert not filtered_df[filtered_df.str.endswith("2022")].empty
 
 
+def test_ratings():
+    """Ensure logical consistency in data, particularly for TV shows with mature ratings."""
+    shows = cleaned_data.loc[cleaned_data["type"] == "TV Show"].loc[cleaned_data["rating"] == "TV-MA"]
+    res = shows["title"].to_list()
+    assert cleaned_data.loc[cleaned_data['title'].isin(res)]["title"].all()
 
 
+def test_country_representation():
+    """Check the geographic diversity of the dataset."""
+    data = cleaned_data["country"].str.split(pat=", ").to_list()
+    countries = []
+    for entity in data:
+        if entity:
+            if type(entity) == list:
+                for country in entity:
+                    countries.append(country)
+            else:
+                countries.append(entity)
+
+    assert len(set(countries)) > 5
